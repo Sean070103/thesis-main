bf5162dd5ed76ac9afffb64d1079caa6c4fd7f07
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-03-30 20:18:32 +0800

    feat: added navbar and other packages

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from '@/components/layouts/Navbar';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
 export default function Home() {
-  return (
-    <>
-      <ConnectButton />
-    </>
-  );
+  return <></>;
 }
